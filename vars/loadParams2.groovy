8bd72902e0e493e08dfe68562f4749625dad29b3
Tomasz Mikulski <tl.mikulski@gmail.com>
2023-05-12 23:38:53 +0200

    2nd implementation

---
+++ vars/loadParams2.groovy
@@ -0,0 +1,23 @@
+def call(configFile) {
+  def libParams = libraryResource "configs/${configFile}.yaml"
+  def cfgParams = readYaml text: libParams
+  def params = []
+
+  cfgParams.each { key, props ->
+    switch(props.type) {
+      case 'booleanParam':
+        params.add(booleanParam(name: key, defaultValue: props.defaultValue, description: props.description))
+      break
+
+      case 'string':
+        params.add(string(name: key, defaultValue: props.defaultValue, description: props.description, trim: props.trim))
+      break
+
+      case 'choice':
+        params.add(choice(name: key, choices: props.choices, description: props.description))
+      break
+    }
+  }
+
+  return params
+}
